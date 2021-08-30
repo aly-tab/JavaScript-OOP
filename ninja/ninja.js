495f9d2a1dc9b9576456904f309b46be9ea130ea
w-phil <wphillips892@gmail.com>
2021-08-30 14:41:48 -0400

    ninja

---
+++ ninja/ninja.js
@@ -0,0 +1,25 @@
+class Ninja {
+    constructor(name, health) {
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+    sayName() {
+        console.log(this.name);
+    }
+    showStats() {
+        console.log("Name " + this.name + "\n"
+        + "Strength " + this.strength + "\n" 
+        + "Speed " + this.speed + "\n" 
+        + "Health " + this.health);
+    }
+    drinkSake() {
+        this.health += 10;
+    }
+}
+
+const ninja1 = new Ninja("Brian", 10);
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
